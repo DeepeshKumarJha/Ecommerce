3f36f314cc7f99e9b82121247af7b185f812e42b
Deepesh Jha <76602335+DeepeshKumarJha@users.noreply.github.com>
2022-02-21 10:55:37 +0530

    nothing major

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,7 +4,7 @@ import NewAndTrendingItems from "./new_arrival_and_trending_list/new_arrival_and
 const Homepage = () => {
 	return (
 		<>
-			<NewAndTrendingItems heading='New Arrival' />
+			<NewAndTrendingItems heading='New Arrivals' />
 			<NewAndTrendingItems heading='Trending' />
 			<CategoryGrid />
 		</>
